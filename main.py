0c44d8335bf93d1e8e2d9a9c406ed72cd1c2b8ad
Diadem Shoukralla <diadem.shoukralla@mail.utoronto.ca>
2021-03-04 13:50:49 -0500

    added sample code for resizing

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from matplotlib import pyplot as plot
 import imageio
 from tkinter import Tk     # from tkinter import Tk for Python 3.x
 from tkinter.filedialog import askopenfilename
+import cv2
 
 Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
 filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
@@ -10,10 +11,12 @@ print(filename)
 
 # filename = "C\:\Users\diade\Documents\School\Spring21\CSC367\recordings\lec5.mp4"
 vid = imageio.get_reader(filename,  'ffmpeg')
-nums = [10, 287]
-for num in nums:
-    image = vid.get_data(num)
-    fig = plot.figure()
-    fig.suptitle('image #{}'.format(num), fontsize=20)
-    plot.imshow(image)
-plot.show(block=False)
+num = 10
+image = vid.get_data(num)
+fig = plot.figure()
+fig.suptitle('image', fontsize=20)
+plot.imshow(image)
+small_image = cv2.resize(image, (100, 100))
+fig.suptitle('image resized', fontsize=20)
+plot.imshow(small_image)
+plot.show()
