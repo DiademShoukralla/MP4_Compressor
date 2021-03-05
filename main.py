1e3a939bd7785bd648d62f4250921386c979890c
Diadem Shoukralla <diadem.shoukralla@mail.utoronto.ca>
2021-03-05 18:04:52 -0500

    used ffmpeg to convert. it works

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
-# import pylab
-from matplotlib import pyplot as plot
-import imageio
 from tkinter import Tk     # from tkinter import Tk for Python 3.x
 from tkinter.filedialog import askopenfilename
-import cv2
+import os
+import subprocess
 
 Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
 filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
-print(filename)
 
-# filename = "C\:\Users\diade\Documents\School\Spring21\CSC367\recordings\lec5.mp4"
-vid = imageio.get_reader(filename,  'ffmpeg')
-num = 10
-image = vid.get_data(num)
-fig = plot.figure()
-fig.suptitle('image', fontsize=20)
-plot.imshow(image)
-small_image = cv2.resize(image, (100, 100))
-fig.suptitle('image resized', fontsize=20)
-plot.imshow(small_image)
-plot.show()
+file_dir = os.path.abspath(filename).rsplit("\\", 1)
+
+compressed_dir = "compressed"
+output_dir = os.path.join(file_dir[0], compressed_dir)
+output_file = os.path.join(output_dir, file_dir[1])
+print(output_file)
+
+subprocess.run(f'ffmpeg -i {filename} -c:v libx264 -s 1440x786 {output_file}')
