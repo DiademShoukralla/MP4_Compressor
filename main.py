c81712e57b34bef119af2393d8c43da65e2df5e1
Diadem Shoukralla <diadem.shoukralla@mail.utoronto.ca>
2021-03-05 20:32:30 -0500

    updated so that it can takes multiple files

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 from tkinter import Tk     # from tkinter import Tk for Python 3.x
-from tkinter.filedialog import askopenfilename
+from tkinter.filedialog import askopenfilenames
 import os
 import subprocess
 
 Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+filenames = askopenfilenames() # show an "Open" dialog box and return the path to the selected file
 
-file_dir = os.path.abspath(filename).rsplit("\\", 1)
-
-compressed_dir = "compressed"
-output_dir = os.path.join(file_dir[0], compressed_dir)
-output_file = os.path.join(output_dir, file_dir[1])
-print(output_file)
-
-subprocess.run(f'ffmpeg -i {filename} -c:v libx264 -s 1440x786 {output_file}')
+for filename in filenames:
+    file_dir = os.path.abspath(filename).rsplit("\\", 1)
+    compressed_dir = "compressed"
+    output_dir = os.path.join(file_dir[0], compressed_dir)
+    output_file = os.path.join(output_dir, file_dir[1])
+    print(output_file)
+    subprocess.Popen(f'ffmpeg -i {filename} -c:v libx264 -s 1440x786 {output_file}')
